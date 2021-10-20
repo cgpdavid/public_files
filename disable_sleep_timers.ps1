e64f14347d7a07a616ecf87fbf66faf2dd3b4cde
cgpdavid <91493264+cgpdavid@users.noreply.github.com>
2021-10-20 18:31:41 -0500

    Create disable_sleep_timers.ps1

---
+++ disable_sleep_timers.ps1
@@ -0,0 +1,13 @@
+function DisableSleepTimers {
+#Disabling sleep on plugged in
+try {
+		write-host "Disabling sleep"
+		Powercfg /x -standby-timeout-ac 0
+		powercfg /x -hibernate-timeout-ac 0
+}
+	catch {
+		write-host "ERROR!" -ForegroundColor Red
+		$_.Exception.Message
+}
+}
+DisableSleepTimers
