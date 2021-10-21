9c637c9cbe81350dc682669ef4675bf3f3da265f
cgpdavid <91493264+cgpdavid@users.noreply.github.com>
2021-10-20 21:00:28 -0500

    Create func_WindowsUpdatesInstallAuto.ps1

---
+++ func_WindowsUpdatesInstallAuto.ps1
@@ -0,0 +1,15 @@
+function WindowsUpdatesInstallAuto {
+try {
+		write-host "Setting up windows updates powershell modules"
+		Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -force
+		Install-PackageProvider -Name NuGet -RequiredVersion 2.8.5.201 -Force
+		Install-Module PSWindowsUpdate -force
+		write-host "Checking for and Installing windows updates - This might take a while"
+		Get-WindowsUpdate -install -acceptall -IgnoreReboot | Format-Table -AutoSize
+		Get-WindowsUpdate -MicrosoftUpdate -Download -Install -AcceptAll -IgnoreReboot
+}
+	catch {
+		write-host "ERROR!" -ForegroundColor Red
+		$_.Exception.Message
+}
+}
