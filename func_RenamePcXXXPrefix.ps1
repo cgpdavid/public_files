fbbbd1567f9c8da14592e2b0534336d98688223d
cgpdavid <91493264+cgpdavid@users.noreply.github.com>
2021-10-21 12:24:53 -0500

    Update func_RenamePcXXXPrefix.ps1

--- func_RenamePcXXXPrefix.ps1
+++ func_RenamePcXXXPrefix.ps1
@@ -1,6 +1,12 @@
+param (
+    [Parameter(Position=0,mandatory=$true)]
+    [string]$ClientHostnamePrefix,
+)
+if(-not($ClientHostnamePrefix)) { Throw “ClientHostnamePrefix has not been defined! for -ClientHostnamePrefix” }
+
 function RenamePcXXXPrefix {
 write-host "Prefix detected was: " ClientHostnamePrefix
-#must set $ClientHostnamePrefix to "xxx"
+#must set  to "xxx"
 $ServiceTAG = (Get-WmiObject Win32_BIOS).serialnumber
 $OLDNAME    = (Get-WmiObject win32_COMPUTERSYSTEM).Name
 Write-Host "Old name is" $OLDNAME
