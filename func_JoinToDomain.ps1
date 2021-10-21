dd6ac7f0725323f4ecbc103f24575ba66d68e18b
cgpdavid <91493264+cgpdavid@users.noreply.github.com>
2021-10-21 12:43:47 -0500

    Update func_JoinToDomain.ps1

--- func_JoinToDomain.ps1
+++ func_JoinToDomain.ps1
@@ -1,4 +1,12 @@
 function JoinToDomain {
+param (
+    [CmdletBinding()]
+    [Parameter(Position=0,mandatory=$true)]
+    [string]$DomainJoinDomain
+)
+
+if(-not($DomainJoinDomain)) { Throw “DomainJoinDomain has not been defined! for -DomainJoinDomain” }
+
 	#Must set $DomainJoinDomain before function! (TLD REQUIRED! cgp.local or ad.cgp.com for example... NOT just "cgp")
 	$dsreg = dsregcmd.exe /status
 	if (($dsreg | Select-String "DomainJoined :") -match "YES") {
@@ -9,8 +17,8 @@ function JoinToDomain {
 	} elseif (($dsreg | Select-String "DomainJoined :") -match "NO"){
 		Write-host "dsregcmd.exe reports that this PC, $env:COMPUTERNAME, is NOT Domain Joined."
 		Write-Verbose "Attempting to Join the domain now - prompting your for domain and credentials"
-		$DomainJoinCred = (read-host "What is your CGP client domain username? (not your internal credentials)")
 		Write-host "Joining domain"
-		add-computer –domainname $DomainJoinDomain -Credential $DomainJoinCred –force
+		write-host "Will Prompt you for credentials"
+		add-computer –domainname $DomainJoinDomain –force
 	}
 }
