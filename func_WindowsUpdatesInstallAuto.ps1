4bacc32d8f7fe37dba857aaa78f533505001d066
cgpdavid <91493264+cgpdavid@users.noreply.github.com>
2021-10-26 14:59:57 -0500

    Update func_WindowsUpdatesInstallAuto.ps1

--- func_WindowsUpdatesInstallAuto.ps1
+++ func_WindowsUpdatesInstallAuto.ps1
@@ -4,6 +4,7 @@ try {
 		Install-PackageProvider -Name NuGet -RequiredVersion 2.8.5.201 -Force
 		Install-Module PSWindowsUpdate -force
 		write-host "Checking for and Installing windows updates - This might take a while"
+		Get-WindowsUpdate
 		Get-WindowsUpdate -install -acceptall -IgnoreReboot | Format-Table -AutoSize
 		Get-WindowsUpdate -MicrosoftUpdate -Download -Install -AcceptAll -IgnoreReboot
 }
