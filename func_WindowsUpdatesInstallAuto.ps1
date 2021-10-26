dfb23b8842c246691772c7950b60fae1c592dab7
cgpdavid <91493264+cgpdavid@users.noreply.github.com>
2021-10-26 15:00:32 -0500

    Update func_WindowsUpdatesInstallAuto.ps1

--- func_WindowsUpdatesInstallAuto.ps1
+++ func_WindowsUpdatesInstallAuto.ps1
@@ -3,7 +3,7 @@ try {
 		write-host "Setting up windows updates powershell modules"
 		Install-PackageProvider -Name NuGet -RequiredVersion 2.8.5.201 -Force
 		Install-Module PSWindowsUpdate -force
-		write-host "Checking for and Installing windows updates - This might take a while"
+		write-host "Checking for and Installing windows updates - This might take a long time"
 		Get-WindowsUpdate
 		Get-WindowsUpdate -install -acceptall -IgnoreReboot | Format-Table -AutoSize
 		Get-WindowsUpdate -MicrosoftUpdate -Download -Install -AcceptAll -IgnoreReboot
