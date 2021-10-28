ec50019551f0c83f1f0cd37119bac34501c18737
cgpdavid <91493264+cgpdavid@users.noreply.github.com>
2021-10-28 16:40:57 -0500

    Update script_bluebeamrevu_install.ps1

--- script_bluebeamrevu_install.ps1
+++ script_bluebeamrevu_install.ps1
@@ -1,5 +1,4 @@
 #BLUEBEAM 
-#CheckAndCreateITSFolders
 remove-item $env:TEMP\func_CheckAndCreateITSFolders.ps1 -erroraction silentlycontinue
 powershell -exec bypass -c "Invoke-WebRequest https://raw.githubusercontent.com/cgpdavid/public_files/main/func_CheckAndCreateITSFolders.ps1 -OutFile $env:TEMP\func_CheckAndCreateITSFolders.ps1"
 powershell -exec bypass -c ". $env:TEMP\func_CheckAndCreateITSFolders.ps1; CheckAndCreateITSFolders"
@@ -16,20 +15,26 @@ if (!(Test-Path "C:\itsupport\Installers\BlueBeam\")){
 	Expand-Archive -LiteralPath 'C:\itsupport\Installers\BlueBeam\MSIBluebeamRevuLATESTx64.zip' -DestinationPath C:\itsupport\Installers\BlueBeam\
 	Copy-Item "C:\itsupport\Installers\BlueBeam\Uninstall Previous Versions.txt" "C:\itsupport\Installers\BlueBeam\Uninstall Previous Versions.bat"
 	write-host "cleaning system of prior Bluebeam versions"
-	start-process "cmd.exe" "/c C:\itsupport\Installers\BlueBeam\Uninstall Previous Versions.bat"
+	start-process "C:\itsupport\Installers\BlueBeam\Uninstall Previous Versions.bat" -Wait
+
 		
+	$fileToCheck = "$Env:Programfiles\Bluebeam Software\Bluebeam Revu\20\Revu\Revu.exe"
+	if (![System.IO.File]::Exists($fileToCheck)){
+		write-host "Bluebeam 20 not detected, continuing"
+		set-location "C:\itsupport\Installers\BlueBeam\"
+		$BB_SERIALNUMBER = Read-Host "What is the SERIAL NUMBER? (usually formated xxxxxxx)"
+		$BB_PRODUCTKEY = Read-Host "What is the PRODUCT KEY? (usually xxxxx-xxxxxxx format)"
+		$BB_EDITION = read-host "What edition of REVU are you installing? (enter 0 for Standard, 1 for CAD, 2 for Extreme)"
+		write-host "Launching Bluebeam installer - this will take at least 5 minutes"
+		msiexec.exe /i "Bluebeam Revu x64 20.msi" BB_SERIALNUMBER=$BB_SERIALNUMBER BB_PRODUCTKEY=$BB_PRODUCTKEY BB_EDITION=$BB_EDITION BB_DESKSHORTCUT=1 BB_DEFAULTVIEWER=1 /qn /L*V! C:\itsupport\logs\bb_revu_install_log.txt
+		Start-Sleep 300
+		write-host "Launching Bluebeam OCR installer - this will take 2-3 minutes"
+		msiexec.exe /i "BluebeamOCR x64 20.msi" /qn /L*V! C:\itsupport\logs\bb_revu_OCR_install_log.txt
+		Start-Sleep 180
+		write-host "DONE exiting"
+		start-sleep 10
 		
-		$fileToCheck = "$Env:Programfiles\Bluebeam Software\Bluebeam Revu"
-		if (![System.IO.File]::Exists($fileToCheck)){
-			
-			$BB_SERIALNUMBER = Read-Host "What is the SERIAL NUMBER?"
-			$BB_PRODUCTKEY = Read-Host "What is the PRODUCT KEY?"
-			$BB_EDITION = read-host "What edition of REVU are you installing? (enter 0 for Standard, 1 for CAD, 2 for Extreme)"
-			Start-Process C:\itsupport\Installers\BbRevuLATEST.exe -ArgumentList "/QN BB_SERIALNUMBER=$BB_SERIALNUMBER BB_PRODUCTKEY=$BB_PRODUCTKEY BB_EDITION=$BB_EDITION /qn"
-			Start-Process msiexec.exe -Wait -ArgumentList '/I "C:\itsupport\Installers\BbRevuLATEST.exe " /quiet /norestart /L*V! $LogPath\cgp_agent_install_log.txt'
-		} else {
-			write-host "Detected Bluebeam in $Env:Programfiles\Bluebeam Software\Bluebeam Revu"
-			
-		}
+	} else {
+		write-host "Not continuing!  Detected Bluebeam in $Env:Programfiles\Bluebeam Software\Bluebeam Revu"
 		
-	
+	}
