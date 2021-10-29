dc100dc14a08872e024a6ba4372f4bf513a50b06
cgpdavid <91493264+cgpdavid@users.noreply.github.com>
2021-10-29 11:18:53 -0500

    Update script_bluebeamrevu_install.ps1

--- script_bluebeamrevu_install.ps1
+++ script_bluebeamrevu_install.ps1
@@ -18,22 +18,25 @@ powershell -exec bypass -c ". $env:TEMP\func_CheckAndCreateITSFolders.ps1; Check
 write-host "INSTALLING BLUEBEAM REVU"
 		
 if (!(Test-Path "C:\itsupport\Installers\BlueBeam\")){
-	Write-Host "Creating Logs directory"
 	New-Item -Path "C:\itsupport\Installers\" -Name "BlueBeam" -ItemType "directory"
+	New-Item -Path "C:\itsupport\Installers\BlueBeam\" -Name "extracted" -ItemType "directory"
 	}
+	
 	write-host "Downloading Bluebeam - Its over 1.2GB typically - this will take a long while"
 	Invoke-WebRequest "https://www.bluebeam.com/MSIdeployx64" -OutFile C:\itsupport\Installers\BlueBeam\MSIBluebeamRevuLATESTx64.zip
 	write-host "Extracting Bluebeam"
-	Expand-Archive -LiteralPath 'C:\itsupport\Installers\BlueBeam\MSIBluebeamRevuLATESTx64.zip' -DestinationPath C:\itsupport\Installers\BlueBeam\
-	Copy-Item "C:\itsupport\Installers\BlueBeam\Uninstall Previous Versions.txt" "C:\itsupport\Installers\BlueBeam\Uninstall Previous Versions.bat"
+	Remove-Item -LiteralPath "C:\itsupport\Installers\BlueBeam\extracted\" -Force -Recurse
+	Expand-Archive -LiteralPath 'C:\itsupport\Installers\BlueBeam\MSIBluebeamRevuLATESTx64.zip' -DestinationPath C:\itsupport\Installers\BlueBeam\extracted\
+	Copy-Item "C:\itsupport\Installers\BlueBeam\extracted\Uninstall Previous Versions.txt" "C:\itsupport\Installers\BlueBeam\extracted\Uninstall Previous Versions.bat"
+	TASKKILL /F /IM "BBPrint.exe"
 	write-host "cleaning system of prior Bluebeam versions"
-	start-process "C:\itsupport\Installers\BlueBeam\Uninstall Previous Versions.bat" -Wait
+	start-process "C:\itsupport\Installers\BlueBeam\extracted\Uninstall Previous Versions.bat" -Wait
 
 		
 	$fileToCheck = "$Env:Programfiles\Bluebeam Software\Bluebeam Revu\20\Revu\Revu.exe"
 	if (![System.IO.File]::Exists($fileToCheck)){
 		write-host "Bluebeam 20 not detected, continuing"
-		set-location "C:\itsupport\Installers\BlueBeam\"
+		set-location "C:\itsupport\Installers\BlueBeam\extracted\"
 		$BB_SERIALNUMBER = Read-Host "What is the SERIAL NUMBER? (usually formated xxxxxxx)"
 		$BB_PRODUCTKEY = Read-Host "What is the PRODUCT KEY? (usually xxxxx-xxxxxxx format)"
 		$BB_EDITION = read-host "What edition of REVU are you installing? (enter 0 for Standard, 1 for CAD, 2 for Extreme)"
