9bb9638c84031d789bb08336bcb77c939382af5d
cgpdavid <91493264+cgpdavid@users.noreply.github.com>
2021-11-01 22:20:19 -0500

    Update script_wipe_bloat_mcafee.ps1

--- script_wipe_bloat_mcafee.ps1
+++ script_wipe_bloat_mcafee.ps1
@@ -96,9 +96,8 @@ function RemoveMcAfeeAppPackages {
 	Write-Output "Removing McAfee Windows Tiles and Apps" | Out-Default
 	$RemoveApp = 'Mcafee'
 	Get-AppxPackage -AllUsers | Where-Object {$_.Name -Match $RemoveApp} | Remove-AppxPackage -AllUsers
-	Get-AppxPackage | Where-Object {$_.Name -Match $RemoveApp} | Remove-AppxPackage -AllUsers
-	Get-AppxProvisionedPackage -Online | Where-Object {$_.PackageName -Match $RemoveApp} | Remove-AppxProvisionedPackage -Online -AllUsers
-	
+	Get-AppxPackage -AllUsers | Where-Object {$_.Name -Match $RemoveApp} | Remove-AppxPackage -AllUsers
+	Get-AppxProvisionedPackage -Online -AllUsers | Where-Object {$_.PackageName -Match $RemoveApp} | Remove-AppxProvisionedPackage -Online -AllUsers
 	
 }
 RemoveMcAfeeAppPackages
