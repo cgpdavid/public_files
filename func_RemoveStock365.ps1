d1187b07e29c189c31815e8a6acfa4eb568d1d88
cgpdavid <91493264+cgpdavid@users.noreply.github.com>
2021-11-01 22:40:56 -0500

    Update func_RemoveStock365.ps1

--- func_RemoveStock365.ps1
+++ func_RemoveStock365.ps1
@@ -19,8 +19,8 @@ write-host "It will likely say some dont exist - this is fine. continue"
 Get-Process -Name 'setup.exe','firstrun.exe','appvshnotify.exe','Officeclicktorun.exe' | Stop-Process -Force -ErrorAction SilentlyContinue
 
 #Delete office tiles in start menu
-Get-appxpackage -allusers *Microsoft.MicrosoftOfficeHub* | Remove-AppxPackage -allusers
-Get-appxprovisionedpackage –online | where-object {$_.packagename –like '*Microsoft.MicrosoftOfficeHub*'} | remove-appxprovisionedpackage –online
+Get-appxpackage -allusers *Microsoft.MicrosoftOfficeHub* -allusers | Remove-AppxPackage -allusers
+Get-appxprovisionedpackage –online | where-object {$_.packagename –like '*Microsoft.MicrosoftOfficeHub*'} | remove-appxprovisionedpackage –online -allusers
 Get-appxpackage -allusers *onenote* | Remove-AppxPackage -allusers
 Get-appxprovisionedpackage –online | where-object {$_.packagename –like '*onenote*'} | remove-appxprovisionedpackage –online -allusers
 
