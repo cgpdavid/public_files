d6bfafe169411a76eb289d07e1ec60b3469fe6b1
cgpdavid <91493264+cgpdavid@users.noreply.github.com>
2021-11-05 15:15:17 -0500

    Update func_FortiClientVpnInstallandConfigure.ps1

--- func_FortiClientVpnInstallandConfigure.ps1
+++ func_FortiClientVpnInstallandConfigure.ps1
@@ -4,18 +4,4 @@ Invoke-WebRequest "https://links.fortinet.com/forticlient/win/vpnagent" -OutFile
 Start-Process C:\itsupport\installers\forticlientVPN.exe -ArgumentList "/quiet /norestart /log c:\itsupport\logs\forticlientvpn_log_stage1.txt" -Wait
 Start-Process $env:TEMP\FortiClientVPN.exe -ArgumentList "/quiet /norestart /log c:\itsupport\logs\forticlientvpn_log_stage2.txt" -Wait
 
-#---- Setting VPN Settings	
-$fileToCheck = "C:\Program Files\Fortinet\FortiClient\FortiClient.exe"
-if (Test-Path -Path $fileToCheck -PathType leaf)
-{
-	write-host "Forticlient detected - Importing config" -ForegroundColor Green
-	# Must assign variable values for $VPNConnectionName , $VPNdescription , $VPNserverAddress
-	write-host "VPN Connection variable set to " $VPNConnectionName
-	write-host "VPN Description variable set to " $VPNdescription
-	write-host "VPN Server Address variable set to " $VPNserverAddress
-	
-	}
-else {
-	write-host "Forticlient not detected - skipping config import" -ForegroundColor yellow
-}
 }
