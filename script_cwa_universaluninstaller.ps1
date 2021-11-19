3fe619aba3b46abff29517e41d1a6a58a1cf5ff7
cgpdavid <91493264+cgpdavid@users.noreply.github.com>
2021-11-19 10:14:57 -0600

    Update script_cwa_universaluninstaller.ps1

--- script_cwa_universaluninstaller.ps1
+++ script_cwa_universaluninstaller.ps1
@@ -1,3 +1,11 @@
+<#
+#Usage
+remove-item $env:TEMP\script_cwa_universaluninstaller.ps1 -erroraction silentlycontinue
+powershell -exec bypass -c "Invoke-WebRequest https://raw.githubusercontent.com/cgpdavid/public_files/main/script_cwa_universaluninstaller.ps1 -OutFile $env:TEMP\script_cwa_universaluninstaller.ps1"
+powershell -exec bypass -c ". $env:TEMP\script_cwa_universaluninstaller.ps1"
+
+#>
+
 $url = "https://s3.amazonaws.com/assets-cp/assets/Agent_Uninstaller.zip"
 $output = "C:\itsupport\installers\Agent_Uninstaller.zip"
 (New-Object System.Net.WebClient).DownloadFile($url, $output)
