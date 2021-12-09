0d753a847fa0bc51bf3f70c62b8cc95418711cd4
cgpdavid <91493264+cgpdavid@users.noreply.github.com>
2021-12-09 11:49:02 -0600

    Update script_printer_troulbleshoot.ps1

--- script_printer_troulbleshoot.ps1
+++ script_printer_troulbleshoot.ps1
@@ -1,42 +1,106 @@
-
 <#
-#Usage
-#func_install_rc_phone
-Set-ExecutionPolicy Bypass -Scope Process -force
-remove-item $env:TEMP\func_install_rc_phone.ps1 -erroraction silentlycontinue
-powershell -exec bypass -c "Invoke-WebRequest https://raw.githubusercontent.com/cgpdavid/public_files/main/func_install_rc_phone.ps1 -OutFile $env:TEMP\func_install_rc_phone.ps1"
-powershell -c ". $env:TEMP\func_install_rc_phone.ps1;"
+#USAGE
+
+#script_printer_troulbleshoot
+remove-item $env:TEMP\script_printer_troulbleshoot.ps1 -erroraction silentlycontinue
+powershell -exec bypass -c "Invoke-WebRequest https://raw.githubusercontent.com/cgpdavid/public_files/main/script_printer_troulbleshoot.ps1 -OutFile $env:TEMP\script_printer_troulbleshoot.ps1"
+powershell -exec bypass -c ". $env:TEMP\script_printer_troulbleshoot.ps1"
 
 #>
 
-#Checking if Ringcentral is installed
-$fileToCheck = "C:\Program Files (x86)\RingCentral\SoftPhoneApp\Softphone.exe"
-if (-not(Test-Path $fileToCheck -PathType leaf)){
-	write-host "RingCentral phone not detected - proceeding"
-	#Setup working enviorment folders
-	remove-item $env:TEMP\func_CheckAndCreateITSFolders.ps1 -erroraction silentlycontinue
-	powershell -exec bypass -c "Invoke-WebRequest https://raw.githubusercontent.com/cgpdavid/public_files/main/func_CheckAndCreateITSFolders.ps1 -OutFile $env:TEMP\func_CheckAndCreateITSFolders.ps1"
-	powershell -exec bypass -c ". $env:TEMP\func_CheckAndCreateITSFolders.ps1; CheckAndCreateITSFolders"
 
-	#Ringcentral touches spooler driver folder for somereason - need to bypass PrintNightmare Just in case
-	Write-Host 'Bypassing Print Nightmare ACL' -ForegroundColor Green
-	$Path = "C:\Windows\System32\spool\drivers"
-	$Acl = (Get-Item $Path).GetAccessControl('Access')
-	$Ar = New-Object System.Security.AccessControl.FileSystemAccessRule("System", "Modify", "ContainerInherit, ObjectInherit", "None", "Deny")
-	$Acl.RemoveAccessRule($Ar)
-	Set-Acl $Path $Acl
 
-	#fetching most recent installer and running it with logging
-	$url = "https://downloads.ringcentral.com/sp/RingCentralForWindows"
-	$outpath = "C:\itsupport\installers\RingCentral-Phone-MostRecent.msi"
-	$ProgressPreference = 'SilentlyContinue'
-	New-Item -ItemType Directory -Force -Path C:\itsupport\installers\
-	Invoke-WebRequest -Uri $url -OutFile $outpath
-	$wc = New-Object System.Net.WebClient
-	$wc.DownloadFile($url, $outpath)
-	Write-Host "FileSource $url"
-	Write-Host "FileDestination $outpath"
-	Start-Process msiexec.exe -Wait -ArgumentList '/I "C:\itsupport\installers\RingCentral-Phone-MostRecent.msi" ALLUSERS=1 /quiet /L*V! C:\itsupport\logs\RingCentralPhone_install_log.txt'
-} else {
-	write-host "Ringcentral phone is already installed - exiting"
+
+do {
+Write-Host 'CGP PRINT TROUBLESHOOTING SCRIPT'
+''
+$askContinue=Read-Host "Continue running this script on $env:computername ? (Y/N)"
+
+''
+If ($askContinue -eq 'Y')
+ 
+
+{
+function CheckIfRunningAsAdmin {
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
+    write-host "MUST BE RUN AS ADMIN - Exiting in 10 seconds!" -ForegroundColor RED
+	sleep 10
+    Exit
+}elseif (([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")) {
+	write-host "This script is running as administrator!" -ForegroundColor GREEN
 }
+}
+CheckIfRunningAsAdmin
+
+$askClearPrintJobs=Read-Host "Would you like to clear Stuck printjobs? (Y/N)"
+$askPrintNightmare=Read-Host "Would you like to bypass PrintNightmare? (Y/N)"
+$askBypassMsKB=Read-Host "Would you like to bypass Microsoft printing as admin KB5005033? (Y/N)"
+$askGpupdate=Read-Host "Would you like to gpupdate /force as the last step? (Y/N)"
+
+
+
+
+'================================================================='
+
+If ($askPrintNightmare -eq 'Y'){
+Write-Host 'Bypassing Print Nightmare ACL' -ForegroundColor Green
+$Path = "C:\Windows\System32\spool\drivers"
+$Acl = (Get-Item $Path).GetAccessControl('Access')
+$Ar = New-Object System.Security.AccessControl.FileSystemAccessRule("System", "Modify", "ContainerInherit, ObjectInherit", "None", "Deny")
+$Acl.RemoveAccessRule($Ar)
+Set-Acl $Path $Acl
+}
+
+
+If ($askBypassMsKB -eq 'Y'){
+Write-Host 'Bypassing Microsoft Driver Install as Admin patch' -ForegroundColor Green
+REG ADD "HKLM\Software\Policies\Microsoft\Windows NT\Printers\PointAndPrint" /v RestrictDriverInstallationToAdministrators /t REG_DWORD /d 0
+REG ADD "HKLM\Software\Policies\Microsoft\Windows NT\Printers\PointAndPrint" /v UpdatePromptSettings /t REG_DWORD /d 0
+REG ADD "HKLM\Software\Policies\Microsoft\Windows NT\Printers\PointAndPrint" /v NoWarningNoElevationOnInstall /t REG_DWORD /d 0
+}
+
+If ($askClearPrintJobs -eq 'Y'){
+Write-Host 'Stopping Spooler Service ...' -ForegroundColor Green
+Stop-Service spooler
+If ((Get-Service spooler).status -eq 'Running')
+{Write-Host 'Error! Spooler could not be stopped!' -ForegroundColor Red}
+
+Write-Host "Clearing printjobs in $env:windir\system32\spool\PRINTERS" -ForegroundColor Green
+Remove-Item -Path $env:windir\system32\spool\PRINTERS\*.*
+
+
+Write-Host 'Starting Spooler Service ...' -ForegroundColor Green
+$start=Start-Service Spooler -ErrorAction Ignore
+If ((Get-Service spooler).status -eq 'Stopped')
+{Write-Host 'Error! Spooler could not be started!' -ForegroundColor Red}
+
+write-host "DONE!"
+}
+
+If ($askGpupdate -eq 'Y'){
+Write-Host 'Forcing GPUPDATE as admin!' -ForegroundColor Green
+gpupdate /force
+
+Write-Host 'Forcing GPUPDATE as logged on user!' -ForegroundColor Green
+$ArgumentList = "/k gpupdate /force & exit"
+$apppath = "cmd.exe" 
+$taskname = "Launch $apppath"
+$action = New-ScheduledTaskAction -Execute $apppath -Argument $ArgumentList
+write-host $action 
+$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date)
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskname | Out-Null
+Start-ScheduledTask -TaskName $taskname
+Start-Sleep -s 1
+Unregister-ScheduledTask -TaskName $taskname -Confirm:$false
+
+}
+write-host "Launching Printers Control Panel then exiting!"
+control printers
+'================================================================='
+''
+write-host "Exiting!"
+PAUSE
+exit
+}
+}
+while ($askContinue -ne 'N')
