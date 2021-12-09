191a5ce909b909583c421e7648c15532754da45c
cgpdavid <91493264+cgpdavid@users.noreply.github.com>
2021-12-09 11:13:00 -0600

    Update func_install_rc_phone.ps1

--- func_install_rc_phone.ps1
+++ func_install_rc_phone.ps1
@@ -1,9 +1,10 @@
 <#
 #Usage
+#func_install_rc_phone
 Set-ExecutionPolicy Bypass -Scope Process -force
-remove-item c:\itsupport\scripts\func_install_rc_phone.ps1 -erroraction silentlycontinue
-powershell -exec bypass -c "Invoke-WebRequest https://raw.githubusercontent.com/cgpdavid/public_files/main/func_install_rc_phone.ps1 -OutFile c:\itsupport\scripts\func_install_rc_phone.ps1"
-powershell -c ". c:\itsupport\scripts\func_install_rc_phone.ps1;"
+remove-item $env:TEMP\func_install_rc_phone.ps1 -erroraction silentlycontinue
+powershell -exec bypass -c "Invoke-WebRequest https://raw.githubusercontent.com/cgpdavid/public_files/main/func_install_rc_phone.ps1 -OutFile $env:TEMP\func_install_rc_phone.ps1"
+powershell -c ". $env:TEMP\func_install_rc_phone.ps1;"
 
 #>
 
@@ -13,15 +14,12 @@ powershell -exec bypass -c ". $env:TEMP\func_CheckAndCreateITSFolders.ps1; Check
 
 
 $url = "https://downloads.ringcentral.com/sp/RingCentralForWindows"
-$outpath = "C:\itsupport\installers\RingCentral-Phone-MostRecent.msi"
+$outpath = "$env:TEMP\RingCentral-Phone-MostRecent.msi"
 $ProgressPreference = 'SilentlyContinue'
-New-Item -ItemType Directory -Force -Path C:\itsupport\installers\
+New-Item -ItemType Directory -Force -Path $env:TEMP\
 Invoke-WebRequest -Uri $url -OutFile $outpath
 $wc = New-Object System.Net.WebClient
 $wc.DownloadFile($url, $outpath)
 Write-Host "FileSource $url"
 Write-Host "FileDestination $outpath"
-Start-Process msiexec.exe -Wait -ArgumentList '/I "C:\itsupport\installers\RingCentral-Phone-MostRecent.msi" ALLUSERS=1 /quiet /L*V! C:\itsupport\logs\RingCentralPhone_install_log.txt'
-
-
-
+Start-Process msiexec.exe -Wait -ArgumentList '/I "$env:TEMP\RingCentral-Phone-MostRecent.msi" ALLUSERS=1 /quiet /L*V! $env:TEMP\RingCentralPhone_install_log.txt'
