f55a1eb8780d1dc0fa6a481a39f908f3b0bb0187
cgpdavid <91493264+cgpdavid@users.noreply.github.com>
2022-01-10 23:20:46 -0600

    Update func_RemoveStock365.ps1

--- func_RemoveStock365.ps1
+++ func_RemoveStock365.ps1
@@ -6,38 +6,47 @@ write-host "This sometimes takes a long time. Upwards of 5 to 10 minutes sometim
 
 
 #Delete scheduled tasks related to 365
-Write-Host "Deleting bundled 365 scheduled tasks"
-Unregister-ScheduledTask -TaskName "Office Automatic Update" -Confirm:$false -erroraction 'silentlycontinue'
-Unregister-ScheduledTask -TaskName "Office Automatic Updates 2.0" -Confirm:$false -erroraction 'silentlycontinue'
-Unregister-ScheduledTask -TaskName "Office ClickToRun Service Monitor" -Confirm:$false -erroraction 'silentlycontinue'
-Unregister-ScheduledTask -TaskName "Office Feature Updates" -Confirm:$false -erroraction 'silentlycontinue'
-Unregister-ScheduledTask -TaskName "Office Feature Updates Logon" -Confirm:$false -erroraction 'silentlycontinue'
-Unregister-ScheduledTask -TaskName "Office Subscription Maintenance" -Confirm:$false -erroraction 'silentlycontinue'
-Unregister-ScheduledTask -TaskName "Clicktorunsvc" -Confirm:$false -erroraction 'silentlycontinue'
-
+Write-Host "Deleting bundled 365 scheduled tasks - this may take a moment"
+$SchedTaskToUnreg = @('Office Automatic Update','Office Automatic Updates 2.0','Office ClickToRun Service Monitor','Office Feature Updates','Office Feature Updates Logon','Office Subscription Maintenance','Clicktorunsvc')
+foreach ($i in $SchedTaskToUnreg) {
+	Unregister-ScheduledTask -TaskName $i -Confirm:$false -erroraction 'silentlycontinue'
+	
+}
+write-host "Done"
 
 #Stop processes potentiall 365 related
 Write-Host "Stopping 365 related processes"
-write-host "It will likely say some dont exist - this is fine. continue"
-Get-Process -Name 'setup.exe','firstrun.exe','appvshnotify.exe','Officeclicktorun.exe' | Stop-Process -Force -ErrorAction SilentlyContinue
+write-host "This may throw some red errors, but you can safely ignore them"
+$ProcessesToKill = @('setup.exe','firstrun.exe','appvshnotify.exe','Officeclicktorun.exe')
+foreach ($i in $ProcessesToKill) {
+	if((get-process "$i" -ea SilentlyContinue) -eq $Null){ 
+	write-host $i " Not detected - continuing"
+		}else {
+			write-host "Found process " $i
+			write-host "Stopping it.."
+			Stop-Process -processname "$i"
+			}
+	
+}
+write-host "Done"
+
 
 #Delete office tiles in start menu
-Get-appxpackage -allusers *Microsoft.MicrosoftOfficeHub* -allusers | Remove-AppxPackage -allusers
+write-host "Removing some office tiles in start menu"
+Get-appxpackage -allusers *Microsoft.MicrosoftOfficeHub* | Remove-AppxPackage -allusers
 Get-appxprovisionedpackage -online | where-object {$_.packagename -like '*Microsoft.MicrosoftOfficeHub*'} | remove-appxprovisionedpackage -online -allusers
 Get-appxpackage -allusers *onenote* | Remove-AppxPackage -allusers
 Get-appxprovisionedpackage -online | where-object {$_.packagename -like '*onenote*'} | remove-appxprovisionedpackage -online -allusers
-
+write-host "Done"
 
 #Attempt uninstall of bundled 365 installs
 Write-Host "Attempting automated uninstall of 365 C2R apps"
 write-host "Checking if bloat c2r installers exist"
 $fileToCheck = "C:\Program Files\Common Files\Microsoft Shared\ClickToRun\OfficeClickToRun.exe"
 
-Show-InstallationProgress -StatusMessage "Performing 365 cleanup. This may take some time. Please wait...";
-Write-Log -Message "365 Will be uninstalled"
+write-host "Performing 365 cleanup. This may take some time. Please wait..."
 powershell -exec bypass -c "Invoke-WebRequest https://raw.githubusercontent.com/OfficeDev/Office-IT-Pro-Deployment-Scripts/master/Office-ProPlus-Deployment/Remove-PreviousOfficeInstalls/OffScrubc2r.vbs -OutFile c:\itsupport\scripts\OffScrubc2r.vbs"
-Execute-Process -FilePath "CScript.Exe" -Arguments "`"$dirsupportFiles\OffScrubc2r.vbs`" CLIENTALL /S /Q /NoCancel" -WindowStyle Hidden -IgnoreExitCodes "1,2,3,16,42"
-
+Start-Process -Filepath cscript.exe -ArgumentList "c:\itsupport\scripts\OffScrubc2r.vbs CLIENTALL /S /Q /NoCancel" -Wait
 
 if (Test-Path $fileToCheck -PathType leaf)
 {
@@ -49,4 +58,5 @@ if (Test-Path $fileToCheck -PathType leaf)
 		Start-Process "C:\Program Files\Common Files\Microsoft Shared\ClickToRun\OfficeClickToRun.exe" -ArgumentList "scenario=install   scenariosubtype=ARP sourcetype=None productstoremove=O365HomePremRetail.16_fr-fr_x-none culture=fr-fr version.16=16.0 DisplayLevel=false" -EA SilentlyContinue -wait
 
 }
+write-host "Done"
 }
